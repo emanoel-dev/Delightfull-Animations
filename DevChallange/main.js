71a3b5633ed75c83cb19b43f8bf7b290ca0d8f42
emanoel-dev <emanoelps09@gmail.com>
2021-10-06 21:04:47 -0300

    melhorias no design e responsividade

---
+++ DevChallange/main.js
@@ -0,0 +1,11 @@
+function btnOpen() {
+    if (document.getElementById('toogle').click) {
+    const menu = document.getElementById('menu')
+    const nav = document.getElementById('nav')
+    menu.classList.toggle('menuBgOpen')
+    nav.classList.toggle('menuOpen')
+
+} else {
+    return
+}
+};
